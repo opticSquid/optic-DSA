87565b31ea075edc0a0fb2437d8860d4622facea
Soumalya Bhattacharya <soumalyabhattacharya6@gmail.com>
2024-02-12 18:21:35 +0530

    Adding the algorithm of Topological Sort

--- Graph/CourseScheduleII.cpp
+++ Graph/CourseScheduleII.cpp
@@ -29,18 +29,10 @@ private:
             }
             else if (pathVis[it])
             {
-                // if (!pathVis[it])
-                // {
-                //     pathTravelled.push_back(it);
-                // }
-                // else
-                // {
-                return true;
-                // }
+                true;
             }
         }
         pathTravelled.push_back(node);
-        // pathTravelled.push_back(node);
         pathVis[node] = 0;
         return false;
     }
@@ -73,12 +65,84 @@ public:
                     {
                         return {};
                     }
-                    // just in case
-                    // all node was not visited in this path
-                    // else if (numCourses != pathTravelled.size())
-                    // {
-                    //     pathTravelled.clear();
-                    // }
+                }
+            }
+        }
+        return pathTravelled;
+    }
+};
+class OptimizedSolution
+{
+private:
+    void convertToAdjList(int numCourses, vector<vector<int>> &prerequisites, vector<vector<int>> &adjList)
+    {
+        for (vector<int> row : prerequisites)
+        {
+            int course = row[0];
+            for (int i = 1; i < row.size(); i++)
+            {
+                adjList[course].push_back(row[i]);
+            }
+        }
+    }
+
+    bool dfsCheck(int node, vector<vector<int>> &adjList, bitset<2> tracked[], vector<int> &pathTravelled)
+    {
+        // visited
+        tracked[node].set(1, 1);
+        // pathVisited
+        tracked[node].set(0, 1);
+        // traverse for adjacent nodes
+        for (auto it : adjList[node])
+        {
+            // when the node is not visited
+            if (tracked[it].count() == 0)
+            {
+                if (dfsCheck(it, adjList, tracked, pathTravelled) == true)
+                    return true;
+            }
+            // node is visited but and also path visited
+            else if (tracked[it].count() == 2)
+            {
+                return true;
+            }
+        }
+        pathTravelled.push_back(node);
+        // unsetting path visited and making it 0
+        tracked[node].set(0, 0);
+        return false;
+    }
+
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>> &prerequisites)
+    {
+        vector<int> pathTravelled;
+        if (prerequisites.empty())
+        {
+            for (int i = 0; i < numCourses; i++)
+            {
+                pathTravelled.push_back(i);
+            }
+        }
+        else
+        {
+            // initially this array has 00 value for all indexes
+            // this array can store 3 values
+            // 0 = in binary 00. first 0 will show not visited, 2nd 0 will show not pathVisited
+            // 2 = in binary 10. first 1 will show visited and 2nd 0 will show not pathVisited
+            // 3 = in binary 11. first 1 will shoe visited, 2nd 1 will show pathVisited
+            //
+            bitset<2> tracked[numCourses];
+            vector<vector<int>> adjList(numCourses, vector<int>({}));
+            convertToAdjList(numCourses, prerequisites, adjList);
+            for (int i = 0; i < numCourses; i++)
+            {
+                if (tracked[i].count() == 0)
+                {
+                    if (dfsCheck(i, adjList, tracked, pathTravelled) == true)
+                    {
+                        return {};
+                    }
                 }
             }
         }
@@ -88,8 +152,8 @@ public:
 int main()
 {
     int numCourses = 2;
-    vector<vector<int>> prerequisites = {{0, 1}};
-    Solution obj;
+    vector<vector<int>> prerequisites = {{1, 0}};
+    OptimizedSolution obj;
     vector<int> result = obj.findOrder(numCourses, prerequisites);
     for (int r : result)
     {

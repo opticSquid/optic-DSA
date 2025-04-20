b921eb7c7c5aeaad50d75cbbf09944268a0566af
Soumalya Bhattacharya <soumalyabhattacharya6@gmail.com>
2025-04-20 11:02:17 +0530

    adds another implementation of the function

--- bit-manipulation/easy/countNumberOfSetBits.cpp
+++ bit-manipulation/easy/countNumberOfSetBits.cpp
@@ -3,15 +3,28 @@ using namespace std;
 class Solution
 {
 public:
-    int count_bits(int n)
+    int count_bits_easy(int n)
     {
         int count = 0;
         while (n >= 1)
         {
-            count += n % 2;
+            // count  = n % 2;
+            count += n & 1;
 
             // n /= 2
             n = n >> 1;
+        }
+        return count;
+    }
+    int count_bits_cmplx(int n)
+    {
+        int count = 0;
+        while (n > 0)
+        {
+            count++;
+            // every time the right most set bit turns to 0;
+            // so we loop until all set bits are turned to 0
+            n = n & (n - 1);
         }
         return count;
     }
@@ -21,6 +34,6 @@ int main()
     int n;
     cin >> n;
     Solution obj;
-    cout << obj.count_bits(n) << endl;
+    cout << obj.count_bits_cmplx(n) << endl;
     return 0;
 }

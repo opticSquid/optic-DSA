65b5b86c8a81f5f8b590e7a84571764fbf3f3bb8
Soumalya Bhattacharya <soumalyabhattacharya6@gmail.com>
2025-05-20 13:35:30 +0530

    optimized solution for largest rectangle in histogram

--- stack-and-queue/monotonic-stack-queue/largest-rectangle-in-histogram.cpp
+++ stack-and-queue/monotonic-stack-queue/largest-rectangle-in-histogram.cpp
@@ -2,58 +2,24 @@
 using namespace std;
 class Solution
 {
-private:
-    vector<int> prevSmallerElementIndex(vector<int> &heights)
-    {
-        int n = heights.size();
-        stack<int> st;
-        vector<int> res(n, -1);
-        for (int i = 0; i < n; i++)
-        {
-            while (!st.empty() && heights[st.top()] >= heights[i])
-            {
-                st.pop();
-            }
-            if (!st.empty())
-            {
-                res[i] = st.top();
-            }
-            st.push(i);
-        }
-        return res;
-    }
-
-    vector<int> nextSmallerElementIndex(vector<int> &heights)
-    {
-        int n = heights.size();
-        stack<int> st;
-        vector<int> res(n, n);
-        for (int i = n - 1; i >= 0; i--)
-        {
-            while (!st.empty() && heights[st.top()] >= heights[i])
-            {
-                st.pop();
-            }
-            if (!st.empty())
-            {
-                res[i] = st.top();
-            }
-            st.push(i);
-        }
-        return res;
-    }
-
 public:
     int largestRectangleArea(vector<int> &heights)
     {
-        vector<int> prevSmallerIdx = prevSmallerElementIndex(heights);
-        vector<int> nextSmallerIdx = nextSmallerElementIndex(heights);
         int n = heights.size();
-        int maxArea = INT_MIN;
-        for (int i = 0; i < n; i++)
+        stack<int> st;
+        int maxArea = 0;
+        heights.push_back(0);
+        for (int i = 0; i <= n; ++i)
         {
-            int width = nextSmallerIdx[i] - prevSmallerIdx[i] - 1;
-            maxArea = max(maxArea, width * heights[i]);
+            while (!st.empty() && heights[i] < heights[st.top()])
+            {
+                int height = heights[st.top()];
+                st.pop();
+
+                int width = st.empty() ? i : i - st.top() - 1;
+                maxArea = max(maxArea, height * width);
+            }
+            st.push(i);
         }
         return maxArea;
     }

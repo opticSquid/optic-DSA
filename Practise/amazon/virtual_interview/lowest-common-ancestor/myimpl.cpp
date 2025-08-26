892c28f77d27e4338d5d5b144a293e67a281dfc9
Soumalya Bhattacharya <soumalyabhattacharya6@gmail.com>
2025-08-26 21:35:20 +0530

    adding bst problems

--- Practise/amazon/virtual_interview/lowest-common-ancestor/myimpl.cpp
+++ Practise/amazon/virtual_interview/lowest-common-ancestor/myimpl.cpp
@@ -11,58 +11,27 @@ struct TreeNode
 
 class Solution
 {
-private:
-    int findCommonAncestor(pair<TreeNode *, TreeNode *> locations, unordered_map<TreeNode *, pair<TreeNode *, int>> reverseTree)
-    {
-        stack<TreeNode *> st;
-        st.push(locations.first);
-        while (!st.empty())
-        {
-            TreeNode *node = st.top();
-            st.pop();
-            pair<TreeNode *, int> parent = reverseTree[node];
-            parent.second = 1;
-            st.push(parent.first);
-            reverseTree[node] = parent;
-        }
-    }
-    pair<TreeNode *, TreeNode *> bfsToReveseAndFindPQ(TreeNode *root, TreeNode *p, TreeNode *q, unordered_map<TreeNode *, pair<TreeNode *, int>> reverseTree)
-    {
-        queue<TreeNode *> queue;
-        queue.push(root);
-        pair<TreeNode *, TreeNode *> res;
-        reverseTree[root] = {nullptr, 0};
-        while (!queue.empty())
-        {
-            TreeNode *node = queue.front();
-            queue.pop();
-            if (node->val == p->val)
-            {
-                res.first = node;
-            }
-            else if (node->val == q->val)
-            {
-                res.second = node;
-            }
-            if (node->left)
-            {
-                reverseTree[node->left] = {node, 0};
-                queue.push(node->left);
-            }
-            if (node->right)
-            {
-                reverseTree[node->right] = {node, 0};
-                queue.push(node->right);
-            }
-        }
-        return res;
-    }
-
 public:
     TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q)
     {
-        unordered_map<TreeNode *, pair<TreeNode *, int>> reverseTree;
-        pair<TreeNode *, TreeNode *> locations = bfsToReveseAndFindPQ(root, p, q, reverseTree);
+        if (root == NULL || root == p || root == q)
+        {
+            return root;
+        }
+        TreeNode *left = lowestCommonAncestor(root->left, p, q);
+        TreeNode *right = lowestCommonAncestor(root->right, p, q);
+        if (left == NULL)
+        {
+            return right;
+        }
+        else if (right == NULL)
+        {
+            return left;
+        }
+        else
+        {
+            return root;
+        }
     }
 };
 int main()

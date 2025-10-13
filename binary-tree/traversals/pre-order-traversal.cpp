17cf446e8e42c3bea2e51ac0b1271f8536d69ed5
Soumalya Bhattacharya <soumalyabhattacharya6@gmail.com>
2025-10-13 13:41:16 +0530

    adding pre-order and in-order traversals in iterative approach

---
+++ binary-tree/traversals/pre-order-traversal.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#include "./shared/tree-node.hpp"
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> preorderTraversal(TreeNode *root)
+    {
+        if (root == nullptr)
+        {
+            return {};
+        }
+        stack<TreeNode *> st;
+        st.push(root);
+        vector<int> res;
+        while (!st.empty())
+        {
+            TreeNode *node = st.top();
+            st.pop();
+            res.push_back(node->val);
+            if (node->right != nullptr)
+                st.push(node->right);
+            if (node->left != nullptr)
+                st.push(node->left);
+        }
+        return res;
+    }
+};
+int main()
+{
+    Solution obj;
+    return 0;
+}

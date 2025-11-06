f267aaa46c9ae4fe8a28cbdbf50e72ed2ab768ef
Soumalya Bhattacharya <soumalyabhattacharya6@gmail.com>
2025-11-06 08:15:28 +0530

    WIP boundary traversal

---
+++ binary-tree/medium-problems/boundary-traversal.cpp
@@ -0,0 +1,115 @@
+#include <bits/stdc++.h>
+#include "../shared/tree-node.hpp"
+using namespace std;
+using Node = TreeNode;
+class Solution
+{
+private:
+    vector<int> boundaryNodes;
+    bool isLeaf(Node *node)
+    {
+        return node->left == nullptr && node->right == nullptr;
+    }
+
+    void addLeftBoundary(Node *node)
+    {
+        Node *curr = node->left;
+        while (curr != nullptr)
+        {
+            if (!isLeaf(curr))
+            {
+                boundaryNodes.push_back(curr->val);
+            }
+            if (curr->left != nullptr)
+            {
+                curr = curr->left;
+            }
+            if (curr->left == nullptr && curr->right != nullptr)
+            {
+                curr = curr->right;
+            }
+        }
+    }
+
+    void addLeaves(Node *node)
+    {
+        if (isLeaf(node))
+        {
+            boundaryNodes.push_back(node->val);
+        }
+        if (node->left != nullptr)
+        {
+            addLeaves(node->left);
+        }
+        if (node->right != nullptr)
+        {
+            addLeaves(node->right);
+        }
+    }
+
+    void addRightBoundary(Node *node)
+    {
+        vector<int>::iterator prevLast = boundaryNodes.end();
+        Node *curr = node->right;
+        while (curr)
+        {
+            if (!isLeaf(curr))
+            {
+                boundaryNodes.push_back(curr->val);
+            }
+            if (curr->right != nullptr)
+            {
+                curr = curr->right;
+            }
+            if (curr->right == nullptr && curr->left != nullptr)
+            {
+                curr = curr->left;
+            }
+        }
+        vector<int>::iterator currentLast = boundaryNodes.end();
+        reverse(prevLast + 1, currentLast);
+    }
+
+public:
+    vector<int> boundaryTraversal(Node *root)
+    {
+        if (root == nullptr)
+        {
+            return boundaryNodes;
+        }
+        if (!isLeaf(root))
+        {
+            boundaryNodes.push_back(root->val);
+        }
+        addLeftBoundary(root);
+        addLeaves(root);
+        addRightBoundary(root);
+        return boundaryNodes;
+    }
+};
+void deleteTree(Node *);
+int main()
+{
+    Node *root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right->left = new Node(6);
+    root->right->right = new Node(7);
+    Solution obj;
+    for (int i : obj.boundaryTraversal(root))
+    {
+        cout << i << ", ";
+    }
+    cout << endl;
+    deleteTree(root);
+    return 0;
+}
+
+void deleteTree(Node *node)
+{
+    deleteTree(node->left);
+    deleteTree(node->right);
+    delete node;
+}

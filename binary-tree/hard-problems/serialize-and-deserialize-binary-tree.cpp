dee992c6e17c0eb663b818ac16278335cc73cf88
Soumalya Bhattacharya <soumalyabhattacharya6@gmail.com>
2025-12-11 17:10:52 +0530

    optimizing serialization and deserialization of binary tree

--- binary-tree/hard-problems/serialize-and-deserialize-binary-tree.cpp
+++ binary-tree/hard-problems/serialize-and-deserialize-binary-tree.cpp
@@ -3,6 +3,23 @@
 using namespace std;
 class Solution
 {
+private:
+    // A helper to manually slice the string by commas
+    // Passing 'pos' by reference (&) updates it automatically for the next call
+    string getNextToken(const string &data, int &pos)
+    {
+        int start = pos;
+        while (pos < data.size() && data[pos] != ',')
+        {
+            pos++;
+        }
+        // Extract the substring
+        string token = data.substr(start, pos - start);
+        // Move past the comma for the next turn
+        pos++;
+        return token;
+    }
+
 public:
     // Encodes a tree to a single string.
     string serialize(TreeNode *root)
@@ -14,17 +31,18 @@ public:
         queue<TreeNode *> q;
         string res;
         q.push(root);
+        res.reserve(10000);
         while (!q.empty())
         {
             TreeNode *node = q.front();
             q.pop();
             if (node == nullptr)
             {
-                res += "null,";
+                res.append("null,");
             }
             else
             {
-                res += to_string(node->val) + ",";
+                res.append(to_string(node->val)).append(",");
                 q.push(node->left);
                 q.push(node->right);
             }
@@ -35,14 +53,17 @@ public:
     // Decodes your encoded data to tree.
     TreeNode *deserialize(string data)
     {
-        if (data == "")
+        if (data.empty())
         {
             return nullptr;
         }
-        stringstream s(data);
-        string str;
-        getline(s, str, ',');
-        TreeNode *root = new TreeNode(stoi(str));
+        int pos = 0;
+        int n = data.size();
+        string val;
+
+        val = getNextToken(data, pos);
+
+        TreeNode *root = new TreeNode(stoi(val));
 
         queue<TreeNode *> q;
         q.push(root);
@@ -51,19 +72,18 @@ public:
             TreeNode *node = q.front();
             q.pop();
 
-            getline(s, str, ',');
+            val = getNextToken(data, pos);
 
-            if (str != "null")
+            if (val != "null")
             {
-                TreeNode *leftNode = new TreeNode(stoi(str));
+                TreeNode *leftNode = new TreeNode(stoi(val));
                 node->left = leftNode;
                 q.push(leftNode);
             }
-
-            getline(s, str, ',');
-            if (str != "null")
+            val = getNextToken(data, pos);
+            if (val != "null")
             {
-                TreeNode *rightNode = new TreeNode(stoi(str));
+                TreeNode *rightNode = new TreeNode(stoi(val));
                 node->right = rightNode;
                 q.push(rightNode);
             }

ea5f1dfda56e7ac6e8492fbd08dc6f5c19df8867
Soumalya Bhattacharya <soumalyabhattacharya6@gmail.com>
2026-06-16 09:51:29 +0530

    Feat: Space optimized frog jump

--- dynamic-programming/1d-dp/frog-jump.cpp
+++ dynamic-programming/1d-dp/frog-jump.cpp
@@ -10,20 +10,20 @@ public:
             return 0;
         }
         int n = heights.size();
-        vector<int> dp(n, INT_MAX);
-        dp[0] = 0;
+        int prev1, prev2 = 0;
         int le, re;
         for (int i = 1; i < n; i++)
         {
-            le = dp[i - 1] + abs(heights[i] - heights[i - 1]);
+            le = prev1 + abs(heights[i] - heights[i - 1]);
             re = INT_MAX;
             if (i > 1)
             {
-                re = dp[i - 2] + abs(heights[i] - heights[i - 2]);
+                re = prev2 + abs(heights[i] - heights[i - 2]);
             }
-            dp[i] = min(le, re);
+            prev2 = prev1;
+            prev1 = min(le, re);
         }
-        return dp[n - 1];
+        return prev1;
     }
 };
 int main()

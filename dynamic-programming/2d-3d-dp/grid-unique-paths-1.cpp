f6011b8a6a8417b5b99861ae6b85259d999fd42c
Soumalya Bhattacharya <soumalyabhattacharya6@gmail.com>
2026-06-28 20:10:29 +0530

    Fix: adding tabulation solution of grid unique paths

--- dynamic-programming/2d-3d-dp/grid-unique-paths-1.cpp
+++ dynamic-programming/2d-3d-dp/grid-unique-paths-1.cpp
@@ -26,8 +26,23 @@ private:
 public:
     int uniquePaths(int m, int n)
     {
-        vector<vector<int>> dp(m, vector<int>(n, -1));
-        return countPaths(m - 1, n - 1, dp);
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        int left, up;
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (i == 0 && j == 0)
+                {
+                    dp[i][j] = 1;
+                    continue;
+                }
+                up = i - 1 >= 0 ? dp[i - 1][j] : 0;
+                left = j - 1 >= 0 ? dp[i][j - 1] : 0;
+                dp[i][j] = up + left;
+            }
+        }
+        return dp[m - 1][n - 1];
     }
 };
 int main()

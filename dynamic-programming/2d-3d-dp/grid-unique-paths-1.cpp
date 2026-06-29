147f455d6ac04b4d67d3322ca291e3af92c20c0f
Soumalya Bhattacharya <soumalyabhattacharya6@gmail.com>
2026-06-29 11:04:10 +0530

    Feat: Space optimized form of grid unique paths 1

--- dynamic-programming/2d-3d-dp/grid-unique-paths-1.cpp
+++ dynamic-programming/2d-3d-dp/grid-unique-paths-1.cpp
@@ -3,46 +3,29 @@
 using namespace std;
 class Solution
 {
-private:
-    int countPaths(int i, int j, vector<vector<int>> &dp)
-    {
-        if (i == 0 && j == 0)
-        {
-            return 1;
-        }
-        if (i < 0 || j < 0)
-        {
-            return 0;
-        }
-        if (dp[i][j] != -1)
-        {
-            return dp[i][j];
-        }
-        int up = countPaths(i - 1, j, dp);
-        int left = countPaths(i, j - 1, dp);
-        return dp[i][j] = up + left;
-    }
-
 public:
     int uniquePaths(int m, int n)
     {
-        vector<vector<int>> dp(m, vector<int>(n, 0));
+        vector<int> prev(n, 0);
+        vector<int> cur(n);
         int left, up;
         for (int i = 0; i < m; i++)
         {
+            fill(cur.begin(), cur.end(), 0);
             for (int j = 0; j < n; j++)
             {
                 if (i == 0 && j == 0)
                 {
-                    dp[i][j] = 1;
+                    cur[j] = 1;
                     continue;
                 }
-                up = i - 1 >= 0 ? dp[i - 1][j] : 0;
-                left = j - 1 >= 0 ? dp[i][j - 1] : 0;
-                dp[i][j] = up + left;
+                up = i - 1 >= 0 ? prev[j] : 0;
+                left = j - 1 >= 0 ? cur[j - 1] : 0;
+                cur[j] = up + left;
             }
+            swap(prev, cur);
         }
-        return dp[m - 1][n - 1];
+        return prev[n - 1];
     }
 };
 int main()

1feec52c5b057623c29586ce8f724f82d5d06380
Soumalya Bhattacharya <soumalyabhattacharya6@gmail.com>
2026-07-07 10:43:43 +0530

    Feat: Adds Unique Grid Path 2 space optimized

--- dynamic-programming/2d-3d-dp/grid-unique-paths-2.cpp
+++ dynamic-programming/2d-3d-dp/grid-unique-paths-2.cpp
@@ -1,8 +1,7 @@
 #include <bits/stdc++.h>
 #include <cassert>
 using namespace std;
-class Solution
-{
+class Solution {
 public:
     int uniquePathsWithObstacles(vector<vector<int>> &obstacleGrid)
     {
@@ -11,26 +10,28 @@ public:
             return 0;
         }
         int m = obstacleGrid.size(), n = obstacleGrid[0].size();
-        vector<vector<int>> dp(m, vector<int>(n, 0));
+        vector<int> prevRow(n, 0), curRow(n);
         int top, left;
         for (int i = 0; i < m; i++)
         {
+            fill(curRow.begin(), curRow.end(), 0);
             for (int j = 0; j < n; j++)
             {
                 if (obstacleGrid[i][j] == 0)
                 {
                     if (i == 0 && j == 0)
                     {
-                        dp[0][0] = 1;
+                        curRow[0] = 1;
                         continue;
                     }
-                    top = i - 1 >= 0 ? dp[i - 1][j] : 0;
-                    left = j - 1 >= 0 ? dp[i][j - 1] : 0;
-                    dp[i][j] = top + left;
+                    top = i - 1 >= 0 ? prevRow[j] : 0;
+                    left = j - 1 >= 0 ? curRow[j - 1] : 0;
+                    curRow[j] = top + left;
                 }
             }
+            swap(prevRow, curRow);
         }
-        return dp[m - 1][n - 1];
+        return prevRow[n - 1];
     }
 };
 int main()
